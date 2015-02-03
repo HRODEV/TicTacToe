a7ff57c6b5699c051195d971d1c4babce6b723f6
barld <bakeds@gmail.com>
2015-02-03 12:55:12 +0100

    diagonalen toegevoegd

--- TicTacToe.Bots/WinBlockerBot.cs
+++ TicTacToe.Bots/WinBlockerBot.cs
@@ -29,6 +29,41 @@ namespace TicTacToe.Bots
                     player.PlayTurn(symbols.ToList().IndexOf(FieldSymbol.Empty), i);
                     return;
                 }
+                //verticaal
+                symbols = new[] { board.Fields[i,0].FieldStatus, board.Fields[i, 1].FieldStatus, board.Fields[i, 2].FieldStatus };
+                if (symbols.Count(s => s == opSymbol) == 2 && symbols.Count(s => s == FieldSymbol.Empty) == 1)
+                {
+                    player.PlayTurn(i, symbols.ToList().IndexOf(FieldSymbol.Empty));
+                    return;
+                }
+
+                //diagonaal 1
+                symbols = new[] { board.Fields[0, 0].FieldStatus, board.Fields[1, 1].FieldStatus, board.Fields[2, 2].FieldStatus };
+                if (symbols.Count(s => s == opSymbol) == 2 && symbols.Count(s => s == FieldSymbol.Empty) == 1)
+                {
+                    for (int j = 0; j < 3;j++ )
+                    {
+                        if(board.Fields[i,i].FieldStatus == FieldSymbol.Empty)
+                        {
+                            player.PlayTurn(i, i);
+                        }
+                    }
+                    return;
+                }
+
+                //diagonaal 2
+                symbols = new[] { board.Fields[2, 0].FieldStatus, board.Fields[1, 1].FieldStatus, board.Fields[0, 2].FieldStatus };
+                if (symbols.Count(s => s == opSymbol) == 2 && symbols.Count(s => s == FieldSymbol.Empty) == 1)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (board.Fields[2-i, i].FieldStatus == FieldSymbol.Empty)
+                        {
+                            player.PlayTurn(2-i, i);
+                        }
+                    }
+                    return;
+                }
             }
 
             base.player_OnTurn(sender, e);
